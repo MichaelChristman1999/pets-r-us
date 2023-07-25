63c3dcee7448876e70e27dce5e1fef0d223d8c0d
Michael Christman <mchristman@my365.bellevue.edu>
2023-07-25 15:15:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,182 +6,182 @@
     Description: This is the index.js file for the pets-r-us repo and provides the background for the Pets-R-Us site. It will establish a server connection and instructing the Node.js environment what modules and files to select and how to use them.
 */
 
-
-//List of modules
 const express = require('express');
 const path = require('path');
-const expressLayouts = require('express-ejs-layouts');
-const pino = require('pino');
-
+const app = express();
 const mongoose = require('mongoose');
+const fs = require('fs');
+const Customer = require('./models/customers');
+const Appointment = require('./models/appointments');
+
+// Set Views
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+const PORT = process.env.PORT || 3000;
+
+// Connecting to Mongo
 const CONN = 'mongodb+srv://mchristman:M0nduckDuckG00se@bellevueuniversity.y9g9tgp.mongodb.net/test';
+
+// Showing Server Connection Messages
 mongoose.connect(CONN).then(() => {
-    console.log('Database connection attempt was successful');
+    console.log('The connection to your MongoDB database was successful');
 }).catch(err => {
     console.log('MongoDB Error: ' + err.message);
 });
 
-//Import statement for customers and appointment schemas
-const Customer = require('./models/customer');
-const Appointment = require('./models/appointments');
-
-//This variable enables access for the application to utilize Express.js
-const app = express();
-
-//Name of the server established for the Pets-R-Us website
-const PORT = process.env.PORT || 3000;
-
-//This will notify the file locations to the router and additionally it will instruct to utilize EJS to create 'views' since the files will be inside the 'views' folder
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(expressLayouts)
-app.set('layout', './layouts/layout')
-
-//Page server routes
-
-//If server receives <petsrus/> or <petsrus/index> the user will be routed to the main landing page
+// View to the Landing page
 app.get('/', (req, res) => {
     res.render('index', {
-        title: 'Pets-R-Us Landing', 
-        message: 'Welcome to the Pets-R-Us website!'
-    })
-});
-app.get('/index', (req, res) => {
-    res.render('index', {
-        title: 'Pets-R-Us Landing', 
-        message: 'Welcome to the Pets-R-Us website!'
-    })
+        title: 'Pets-R-Us: Home Page',
+        message: 'Pets-R-Us: Home Page'
+    });
 });
 
-//If server receives <petsrus/groomiing> the user will be routed to the grooming page
+// View to the Grooming page
 app.get('/grooming', (req, res) => {
     res.render('grooming', {
-        title: 'Pets-R-Us Grooming Service',
-        message: 'Grooming Appointments Page'
-    })
-})
-
-//If server receives <petsrus/training> the user will be routed to the training page
-app.get('/training', (req, res) => {
-    res.render('training', {
-        title: 'Pets-R-Us Training',
-        message: 'Training Appointments Page'
-    })
-})
-
-//If server receives <petsrus/boarding> the user will be routed to the boarding page
-app.get('/boarding', (req, res) => {
-    res.render('boarding', {
-        title: 'Pets-R-Us Boarding Service',
-        message: 'Boarding Appointments Page'
-    })
-})
-
-app.get('/customers', (req, res) => {
-    Customer.find({}, function(err, customers){
-        if (err) {
-            console.log(err);
-            next(err);
-        } else {
-            res.render('customer-list', {
-                title: 'Pets-R-Us Customer List',
-                message: 'Customer List',
-                customers: customers
-            })
-        }
-    })   
-})
-
-
-//If server receives <petsrus/registration> the user will be routed to the registration page
-app.get('/registration', (req, res) => {
-    res.render('registration', {
-        title: 'Pets-R-Us User Registration',
-        message: 'User Registration Page'
-    })
-})
-
-app.post('/register', (req, res, next) => {
-    console.log(req.body);
-    console.log(req.body.customerId);
-    console.log(req.body.email);
-    const newCustomer = new Customer({
-      customerId: req.body.customerId,
-      email: req.body.email,
+        title: 'Pets-R-Us: Grooming',
+        message: 'Pets-R-Us: Grooming'
     });
-
-    console.log(newCustomer);
-
-    Customer.create(newCustomer, function(err, cus) {
-        if (err) {
-            console.log(err);
-            next(err);
-        } else {
-            res.render('index', {
-                title: 'Welcome to the Pets-R-Us website!'
-            })
-        }
-    })
-})
-
-app.get('/appointments', (req, res) => {
-    let jsonFile = fs.readFileSync('./public/data/services.json');
-    let services = JSON.parse(jsonFile);
-
-    console.log(services);
-
-    res.render('appointments', {
-        title: 'Pets-R-Us Appointment Booking', 
-        message: 'Book with Pets-R-Us!',
-        services: services
-    })
 });
 
-app.post('/appointment-booked', (req, res, next) => {
-    const newAppointment = new Appointment({
+// View to the Boarding page
+app.get('/boarding', (req, res) => {
+    res.render('boarding', {
+        title: 'Pets-R-Us: Boarding',
+        message: 'Pets-R-Us: Boarding'
+    });
+});
+
+// View to the Training page
+app.get('/training', (req, res) => {
+    res.render('training', {
+        title: 'Pets-R-Us: Training',
+        message: 'Pets-R-Us: Training'
+    });
+});
+
+// View to the Registration page
+app.get('/registration', (req, res) => {
+    res.render('registration', {
+        title: 'Pets-R-Us: Registration',
+        message: 'Pets-R-Us: Registration'
+    });
+});
+
+// HTTP GET route to display the customer list EJS page
+app.get('/customers', async (req, res, next) => {
+    try {
+        const customers = await Customer.find({});
+        res.render('customer-list', {
+            title: 'Pets-R-Us: Customer List',
+            pageTitle: 'Our Customers',
+            customers: customers
+        });
+    } catch (err) {
+        console.log(err);
+        next(err);
+    }
+});
+
+// HTTP Post to the Registration
+app.post('/registrations', async (req, res, next) => {
+    console.log(req.body);
+    const newCustomer = new Customer({
         customerId: req.body.customerId,
         firstName: req.body.firstName,
         lastName: req.body.lastName,
+        streetAddress: req.body.streetAddress,
+        city: req.body.city,
+        state: req.body.state,
+        zipCode: req.body.zipCode,
         email: req.body.email,
-        service: req.body.service,
-    })
-    Appointment.create(newAppointment, function(err, appointment){
-        if (err) {
+        agree: req.body.agree
+    });
+    
+    // If successful, route to Home page otherwise show error
+    const customer = await Customer.create(newCustomer);
+
+    // Converted callback to Async/Await
+    if (customer) {
+        res.render('index', {
+            title: 'Pets-R-Us'
+        });
+        } else {
             console.log(err);
             next(err);
+    }
+  });
+
+// View to the booking page and pulls the services for the form from services.json file
+app.get('/booking', (req, res) => {
+    let jsonFile = fs.readFileSync('./public/data/services.json');
+    let services = JSON.parse(jsonFile);
+  
+    res.render('booking', {
+        title: 'Pets-R-Us: Booking',
+        message: 'Pets-R-Us: Booking',
+        services: services
+    });
+  });
+
+// HTTP Post to the Appointments
+app.post('/booking', async (req, res, next) => {
+    console.log(req.body);
+    const newAppointment = new Appointment({
+        userName: req.body.userName,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        service: req.body.service
+    });
+    
+    // If successful, route to Home page otherwise show error
+    const appointment = await Appointment.create(newAppointment);
+
+    // Converted callback to Async/Await
+    if (appointment) {
+        res.render('index', {
+            title: 'Pets-R-Us'
+        });
         } else {
-            res.render('index',{
-                title: 'Welcome to Pets-R-Us!'
-            })
-        }
-    })
-})
+            console.log(err);
+            next(err);
+    }
+  });
 
 
-///If server receives <petsrus/my-appointments> the user will be routed to the currently booked appointments page
-
-app.get('/my-appointments', (req, res) => {
+// View to the My Appointments Page
+  app.get('/my-appointments', (req, res) => {
     res.render('my-appointments', {
-        title: 'Pets-R-Us Appointments',
-        message: 'Here are the Appointments You Booked'
-    })
-})
+        title: 'Pets-R-Us: My Appointments',
+        message: 'Appointment Lookup'
+    });
+  });
 
-app.get('/api/appointments/:email', async(req, res, next) => {
-    Appointment.find({ email: req.params.email}, function(err, appointments) {
-        if (err) {
-            console.log(err);
-            next(err);
-        } else {
-            res.json(appointments);
-        }
-    })
-})
+  
 
-//The app will listen to the PORT server that was created on line 31
+
+
+  app.get('/api/appointments/:email', async (req, res, next) => {
+    try {
+      const appointments = await Appointment.find({ email: req.params.email });
+      res.json(appointments);
+    } catch (err) {
+      console.log(err);
+      next(err);
+    }
+  });
+  
+  app.use((req, res, next) => {
+    res.status(404).send("Sorry, the page wasn't found")
+  });
+  
 app.listen(PORT, () => {
     console.log('Application started and listening on PORT ' + PORT);
 });
