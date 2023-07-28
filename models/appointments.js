61d402282763dc2c19f31a26173eb58d5beee586
Michael Christman <mchristman@my365.bellevue.edu>
2023-07-28 09:07:05 -0500

    Update appointments.js

--- models/appointments.js
+++ models/appointments.js
@@ -1,18 +1,13 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-// Creating a new mongoose model named Customer
-let customerSchema = new Schema({
-    customerId: { type: String, required: true, unique: true },
+let appointmentSchema = new Schema({
+    userName: { type: String, required: true, unique: true },
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
-    streetAddress: { type: String, required: true },
-    city: { type: String, required: true },
-    state: { type: String, required: true },
-    zipCode: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    agree: { type: String }
+    service: { type: String, required: true}
 });
 
 // Exporting the model
-module.exports = mongoose.model('Customer', customerSchema);
+module.exports = mongoose.model('Appointment', appointmentSchema);
